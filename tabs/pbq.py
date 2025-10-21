ec411efd2a6f671d899d4bd55e84a9db1fa51ddc
Siddhant Ghosh <swaggersiddhant109@gmail.com>
2025-10-21 11:55:53 +0530

    fixed bugs in pbq

--- tabs/pbq.py
+++ tabs/pbq.py
@@ -93,7 +93,7 @@ def show_pbq():
             INSERT INTO pbq_results (email, date, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10,
                                    q11, q12, q13, q14, q15, q16, q17, q18, q19, q20,
                                    q21, q22, q23, q24, q25, total_score, interpretation)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
         """, (email, today, *q_scores, total_score, interpretation))
         con.close()
 
